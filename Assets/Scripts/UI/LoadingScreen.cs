470e3a402a93765a9fb1e09409e893798e05bb4c
MoonLoaf <oskarw.99@hotmail.com>
2022-10-05 14:21:33 +0200

    loading screen shittery

--- Assets/Scripts/UI/LoadingScreen.cs
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -1,16 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadingScreen : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private Image _loadingScreen;
+    [SerializeField] private TMP_Text _loadingText;
+
+
+    private void Start()
     {
-        
+        _loadingText = GetComponentInChildren<TMP_Text>();
+        _loadingScreen = GetComponent<Image>();
+
+        StartCoroutine(LoadingScreenImage());
     }
 
-    void Update()
+    IEnumerator LoadingScreenImage()
     {
-        
+        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForEndOfFrame();
+
+        _loadingText.enabled = false;
+        _loadingScreen.enabled = false;
+
     }
-}
+
+}   
